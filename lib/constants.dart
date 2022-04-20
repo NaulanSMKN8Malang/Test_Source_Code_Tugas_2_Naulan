8b71c60b08bcbbf99fe07977e45f37e4ff311678
NaulanSMKN8Malang <naulansaja@gmail.com>
2022-04-20 12:35:29 +0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,56 @@
+const ELECTRONIC_DATA = [
+  {
+    "name": "Oppo R15",
+    "brand": "Oppo",
+    "price": 1345.95,
+    "image": "oppo_r15.png"
+  },
+  {
+    "name": "Oppo R17",
+    "brand": "Oppo",
+    "price": 1500.36,
+    "image": "oppo_r17.png"
+  },
+  {
+    "name": "Poco F1",
+    "brand": "Xiaomi Pocophone",
+    "price": 2000.00,
+    "image": "poco_f1.png"
+  },
+  {
+    "name": "Realme U1",
+    "brand": "Realme",
+    "price": 1842.30,
+    "image": "realme_u1.png"
+  },
+  {
+    "name": "Redmi 8",
+    "brand": "Xiaomi Redmi",
+    "price": 1200.95,
+    "image": "redmi_8.png"
+  },
+  {
+    "name": "Redmi Note 8 Pro",
+    "brand": "Xiaomi Redmi",
+    "price": 1859.95,
+    "image": "redmi_note_8_pro.png"
+  },
+  {
+    "name": "Vivo V9",
+    "brand": "Vivo",
+    "price": 1500.00,
+    "image": "vivo_v9.png"
+  },
+  {
+    "name": "Vivo Y15",
+    "brand": "Vivo",
+    "price": 1600.50,
+    "image": "vivo_y15.png"
+  },
+  {
+    "name": "Xiaomi Redmi S2",
+    "brand": "Xiaomi Redmi",
+    "price": 1100.00,
+    "image": "xiaomi_redmi_s2.png"
+  }
+];
